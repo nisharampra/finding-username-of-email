4c360a673a6079279344742a3403c11ca12832de
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-09 00:17:35 +0800

    Add files via upload

---
+++ finding username of email/finding username of email/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  finding username of email
+//
+//  Created by Nisha Ramprasath on 8/11/23.
+//
+#include<iostream>
+using namespace std;
+    
+int main()
+{
+    string email="john123@gmail.com";
+    int i=(int)email.find('@');
+    string uname=email.substr(0,i);
+        
+    cout<<"User Name is "<<uname<<endl;
+    
+    return 0;
+        
+}
+    
